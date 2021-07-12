cc7300eee75e638ef1d44c5d6923e58319b8ecd6
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-07-12 15:45:55 -0400

    update build

--- source/elements/form.js
+++ source/elements/form.js
@@ -1,3 +1,5 @@
+import form from '../templates/form.js';
+
 export class Form extends Quantum {
     #form = this.shadowRoot.querySelector('form');
 
@@ -9,3 +11,5 @@ export class Form extends Quantum {
         this.#form.submit();
     }
 }
+
+Form.define('quantum-form', form);
--- source/main.js
+++
@@ -1,4 +0,0 @@
-import { Form } from './elements/form.js';
-import form from './templates/form.js';
-
-Form.define('quantum-form', form);
